c60cd7f9ef8e0b59a0a41c87caca1fa4de5d33bb
Cleve Ard <clevecom2@carolina.rr.com>
2013-12-23 13:31:20 -0500

    Started Camlin's MangaTracker project
    Added ListActity and ListFragment to display the book list

---
+++ MangaTracker/src/com/camlinard/mangatracker/ListFragment.java
@@ -0,0 +1,33 @@
+package com.camlinard.mangatracker;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.os.Bundle;
+
+/**
+ * @author Camlin
+ *
+ */
+public class ListFragment extends Fragment {
+
+	/**
+	 * 
+	 */
+	public ListFragment() {
+		super();
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		ListView view = (ListView)inflater.inflate(R.layout.list_layout, container, false);
+		view.setAdapter(new ArrayAdapter<String>(getActivity(), R.layout.book_layout, books));
+		return view;
+	}
+	
+	public static String[] books = {"a", "b", "c"};
+}
