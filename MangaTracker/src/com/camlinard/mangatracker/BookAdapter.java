0ffd66b29a15cbbe72ac2232d40ac0faab55cf2f
Cleve Ard <clevecom2@carolina.rr.com>
2014-01-20 16:32:55 -0500

    Start of thumbnail code

--- MangaTracker/src/com/camlinard/mangatracker/BookAdapter.java
+++ MangaTracker/src/com/camlinard/mangatracker/BookAdapter.java
@@ -3,10 +3,12 @@ package com.camlinard.mangatracker;
 import java.util.List;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class BookAdapter extends ArrayAdapter<Book> {
@@ -25,6 +27,9 @@ public class BookAdapter extends ArrayAdapter<Book> {
 		text.setText(book.mTitle);
 		text = (TextView)view.findViewById(R.id.book_subtitle);
 		text.setText(book.mSubTitle);
+		ImageView thumb = (ImageView)view.findViewById(R.id.book_list_thumb);
+		Uri uri = Uri.parse(book.mSmallThmbnail);
+		thumb.setImageURI(uri);
 		return view;
 	}
 
