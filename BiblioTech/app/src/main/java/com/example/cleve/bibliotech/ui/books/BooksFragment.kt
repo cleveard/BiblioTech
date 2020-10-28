14091e9aeb857aa4c3bd17cef862c7c3b27756ef
Cleve Ard <clevecom2@carolina.rr.com>
2020-10-28 16:56:13 -0400

    Add UI to order books

--- BiblioTech/app/src/main/java/com/example/cleve/bibliotech/ui/books/BooksFragment.kt
+++ BiblioTech/app/src/main/java/com/example/cleve/bibliotech/ui/books/BooksFragment.kt
@@ -52,7 +52,6 @@ class BooksFragment : Fragment() {
                     child.setOnClickListener(clickHandler)
                     actionButtons[child.id] = child
                 }
-                setActionClickListener(child)
             }
         }
     }
