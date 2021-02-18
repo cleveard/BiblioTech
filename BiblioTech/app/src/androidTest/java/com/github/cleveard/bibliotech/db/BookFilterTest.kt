963f57d9a36ffefb14918d216875e5e2d0aff3f8
Cleve Ard <clevecom2@carolina.rr.com>
2021-02-18 16:28:08 -0500

    Add Single Order test and use debug databse for filter tests
    We order by all columns both ascending and descending. We build the filter and then get the ordered books. First we check that the order of the books matches the order requested. Then we resort the books and include the book id as the final sort key. We also sort the expected books and verify that the lists are the same.
    
    To sort the books and check the order we build a sequence of order lambdas that we interogate. We use the + operator for sequences add in the book id as the final sort key.
    
    We also need to expand the expected book sequence to allow for the author, category and tag joins. We do this by mapping the books to a sequence of book sequences. For the non join columns the book sequence is just the book itself. For the join colums the booksequence is created by mapping the join list to a sequence of books. I needed to special case empty lists, which still include a single book. The sequence of book sequences is then flattened to a sequence of books.

--- BiblioTech/app/src/androidTest/java/com/github/cleveard/bibliotech/db/BookFilterTest.kt
+++ BiblioTech/app/src/androidTest/java/com/github/cleveard/bibliotech/db/BookFilterTest.kt
@@ -5,12 +5,9 @@ import android.content.Context
 import androidx.paging.PagingSource
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
 import com.github.cleveard.bibliotech.R
-import com.github.cleveard.bibliotech.testutils.BookDbTracker
 import com.github.cleveard.bibliotech.testutils.compareBooks
 import com.google.common.truth.StandardSubjectBuilder
-import com.google.common.truth.Truth.assertThat
 import com.google.common.truth.Truth.assertWithMessage
 import kotlinx.coroutines.runBlocking
 import org.junit.*
@@ -31,13 +28,15 @@ class BookFilterTest {
     private lateinit var monthFormat: DateFormat
     private lateinit var yearFormat: DateFormat
     private lateinit var repo: BookRepository
+    private lateinit var contents: List<BookAndAuthors>
 
     @Before
     fun startUp() {
         context = ApplicationProvider.getApplicationContext()
         //context = InstrumentationRegistry.getInstrumentation().context
-        BookRepository.initialize(context, true)
+        BookRepository.initialize(context)
         repo = BookRepository.repo
+        contents = getContents()
         val locale = context.resources.configuration.locales[0]
         dayFormat = DateFormat.getDateInstance(DateFormat.SHORT, locale)
         monthFormat = SimpleDateFormat(context.resources.getString(R.string.month_year_pattern), locale)
@@ -51,10 +50,11 @@ class BookFilterTest {
 
     @Test(timeout = 25000L) fun testEmptySingleFilterFieldNoOrder() {
         runBlocking {
-            val expected = BookDbTracker.addBooks(repo, 8832156L, "Test Empty Filters", 40)
+            assertWithMessage("Test Empty").that(contents.size).isGreaterThan(39)
+            //val expected = BookDbTracker.addBooks(repo, 8832156L, "Test Empty Filters", 40)
             for (c in columnGetValue) {
                 for (p in predicateValue) {
-                    testFilter("Empty", expected.bookEntities.entities, Pair(c, p)) { _, _ -> emptyArray() }
+                    testFilter("Empty", contents.asSequence(), Pair(c, p)) { _, _ -> emptyArray() }
                 }
             }
         }
@@ -62,18 +62,22 @@ class BookFilterTest {
 
     @Test(timeout = 25000L) fun testSingleFilterFieldNoOrder() {
         runBlocking {
-            val expected = BookDbTracker.addBooks(repo, 8832156L, "Test Empty", 40)
+            assertWithMessage("Test Empty").that(contents.size).isGreaterThan(39)
+            //val expected = BookDbTracker.addBooks(repo, 8832156L, "Test Empty", 40)
+            val random = Random(8832156L)
             for (c in columnGetValue) {
                 for (p in predicateValue) {
-                    testFilter("Random", expected.bookEntities.entities, Pair(c, p)) { column, predicate ->
+                    testFilter("Random", contents.asSequence(), Pair(c, p)) { column, predicate ->
                         val values = ArrayList<String>()
-                        repeat(expected.random.nextInt(1, 4)) {
-                            var v: String?
-                            do {
-                                val book = expected.bookEntities[expected.random.nextInt(expected.bookEntities.size)]
-                                v = predicate.modifyString.invoke(column.oneValue.invoke(book, expected.random), expected.random)
-                            } while (v.isNullOrEmpty() || values.contains(v))
-                            values.add(v)
+                        val count = random.nextInt(1, 5)
+                        repeat(10) {
+                            if (values.size < count) {
+                                val v: String?
+                                val book = contents[random.nextInt(contents.size)]
+                                v = predicate.modifyString.invoke(column.oneValue.invoke(book, random), random)
+                                if (!v.isNullOrEmpty() && !values.contains(v))
+                                    values.add(v)
+                            }
                         }
                         values.toTypedArray()
                     }
@@ -82,10 +86,47 @@ class BookFilterTest {
         }
     }
 
+    @Test(timeout = 25000L) fun testSingleOrderFieldNoFilter() {
+        runBlocking {
+            for (c in columnGetValue) {
+                for (o in Order.values()) {
+                    testOrder("Single", contents.asSequence(), Pair(c, o))
+                }
+            }
+        }
+    }
+
     private fun StandardSubjectBuilder.compare(books: List<BookAndAuthors>, expected: List<BookAndAuthors>) {
-        that(books.size).isEqualTo(expected.size)
-        for ((i, b) in books.withIndex())
-            compareBooks(b, expected[i])
+        withMessage("Compare Books %s, %s", books.size, expected.size).apply {
+            var i = 0
+            var j = 0
+            repeat(books.size.coerceAtMost(expected.size)) {
+                compareBooks(books[i++], expected[j++])
+            }
+            if (i < books.size)
+                compareBooks(books[i], null)
+            else if (j < expected.size)
+                compareBooks(null, expected[j])
+        }
+    }
+
+    private fun orderBooks(b1: BookAndAuthors, b2: BookAndAuthors, order: Sequence<(BookAndAuthors, BookAndAuthors) -> Int>): Int {
+        order.forEach {
+            val o = it(b1, b2)
+            if (o != 0)
+                return o
+        }
+        return 0
+    }
+
+    private fun StandardSubjectBuilder.checkOrder(books: List<BookAndAuthors>, order: Sequence<(BookAndAuthors, BookAndAuthors) -> Int>) {
+        if (books.isNotEmpty()) {
+            for (i in 1 until books.size) {
+                withMessage("Check Order %s", i).apply {
+                    that(orderBooks(books[i - 1], books[i], order)).isAtMost(0)
+                }
+            }
+        }
     }
 
     private fun testFilter(label: String, seq: Sequence<BookAndAuthors>, vararg filter: Pair<ColumnValue, PredicateValue>, getValues: (ColumnValue, PredicateValue) -> Array<String>) {
@@ -103,6 +144,38 @@ class BookFilterTest {
         val books = getContents(bookFilter).apply { sortBy { it.book.id } }
         val expected = ArrayList<BookAndAuthors>().apply { addAll(sequence) ; sortBy { it.book.id } }
         assertWithMessage(message.toString()).compare(books, expected)
+    }
+
+    private fun testOrder(label: String, seq: Sequence<BookAndAuthors>, vararg order: Pair<ColumnValue, Order>) {
+        val message = StringBuilder("$label Order ")
+        val list = ArrayList<OrderField>()
+        val sortOrder = ArrayList<BookAndAuthors.(BookAndAuthors) -> Int>()
+        var sequence = seq
+        for (o in order) {
+            message.append("${o.first.column}, ${o.second} ")
+            list.add(OrderField(o.first.column, o.second, false))
+            sortOrder.add(if (o.second == Order.Ascending)
+                o.first.orderCompare
+            else {
+                { -o.first.orderCompare.invoke(this, it) }
+            })
+            sequence = o.first.orderSequence.invoke(sequence)
+        }
+
+        assertWithMessage(message.toString()).apply {
+            val bookFilter = BookFilter(list.toTypedArray(), emptyArray())
+            val books = getContents(bookFilter)
+            val sortSequence = sortOrder.asSequence()
+            checkOrder(books, sortSequence)
+            val withId = sortSequence + sequenceOf({ this.book.id.compareTo(it.book.id) })
+            books.sortWith {b1, b2 -> orderBooks(b1, b2, withId) }
+            val expected = ArrayList<BookAndAuthors>().apply { addAll(sequence) ; sortWith {b1, b2 -> orderBooks(b1, b2, withId) } }
+            compare(books, expected)
+        }
+    }
+
+    private fun getContents(): ArrayList<BookAndAuthors> {
+        return getContents(repo.getBooks())
     }
 
     private fun getContents(filter: BookFilter): ArrayList<BookAndAuthors> {
@@ -140,7 +213,9 @@ class BookFilterTest {
     data class ColumnValue(
         val column: Column,
         val oneValue: BookAndAuthors.(Random) -> String?,
-        val allValues: BookAndAuthors.() -> Sequence<Any?>
+        val orderCompare: BookAndAuthors.(BookAndAuthors) -> Int,
+        val orderSequence: Sequence<BookAndAuthors>.() -> Sequence<BookAndAuthors> = { this },
+        val allValues: BookAndAuthors.() -> Sequence<Sequence<Any?>>,
     )
 
     data class PredicateTests(
@@ -153,7 +228,7 @@ class BookFilterTest {
     data class PredicateValue(
         val predicate: Predicate,
         val modifyString: String?.(Random) -> String?,
-        val test: Sequence<Any?>.(Array<String>) -> Boolean
+        val test: Sequence<Sequence<Any?>>.(Array<String>) -> Boolean
     )
 
     private fun AuthorEntity.lastFirst(): String {
@@ -179,57 +254,112 @@ class BookFilterTest {
         return v
     }
 
-    val columnGetValue: Array<ColumnValue> = arrayOf (
+    private fun String?.compareWith(other: String?, ignoreCase: Boolean = false): Int {
+        return when {
+            this === other -> 0
+            this == null -> -1
+            other == null -> 1
+            else -> this.compareTo(other, ignoreCase)
+        }
+    }
+
+    private val columnGetValue: Array<ColumnValue> = arrayOf (
         // Place holder - actual entry is set below
-        ColumnValue(Column.ANY, { null }) { emptySequence() },
-        ColumnValue(Column.FIRST_NAME, { if (authors.isNullOrEmpty()) null else authors[it.nextInt(authors.size)].name }) {
-            authors.asSequence().map { it.name }
+        ColumnValue(Column.ANY, { null }, { 0 }) { emptySequence() },
+        ColumnValue(Column.FIRST_NAME, { if (authors.isNullOrEmpty()) null else authors[it.nextInt(authors.size)].name },
+            {
+                var o = this.sortFirst.compareWith(it.sortFirst, true)
+                if (o == 0)
+                    o = this.sortLast.compareWith(it.sortLast, true)
+                o
+            },
+            {
+                map {book->
+                    if (book.authors.isEmpty())
+                        sequenceOf(book.copy(sortFirst = null, sortLast = null))
+                    else
+                        book.authors.asSequence().map { book.copy(sortFirst = it.remainingName, sortLast = it.lastName) }
+                }.flatten()
+            }) {
+            sequenceOf(authors.asSequence().map { it.name })
         },
-        ColumnValue(Column.LAST_NAME, { if (authors.isNullOrEmpty()) null else authors[it.nextInt(authors.size)].lastFirst() }) {
-            authors.asSequence().map { it.lastFirst() }
+        ColumnValue(Column.LAST_NAME, { if (authors.isNullOrEmpty()) null else authors[it.nextInt(authors.size)].lastFirst() },
+            {
+                var o = this.sortLast.compareWith(it.sortLast, true)
+                if (o == 0)
+                    o = this.sortFirst.compareWith(it.sortFirst, true)
+                o
+            },
+            {
+                map {book->
+                    if (book.authors.isEmpty())
+                        sequenceOf(book.copy(sortFirst = null, sortLast = null))
+                    else
+                        book.authors.asSequence().map { book.copy(sortFirst = it.remainingName, sortLast = it.lastName) }
+                }.flatten()
+            }) {
+            sequenceOf(authors.asSequence().map { it.lastFirst() })
         },
-        ColumnValue(Column.CATEGORIES, { if (categories.isNullOrEmpty()) null else categories[it.nextInt(categories.size)].category }) {
-            categories.asSequence().map { it.category }
+        ColumnValue(Column.CATEGORIES, { if (categories.isNullOrEmpty()) null else categories[it.nextInt(categories.size)].category },
+            { this.sortCategory.compareWith(it.sortCategory, true) },
+            {
+                map {book->
+                    if (book.categories.isEmpty())
+                        sequenceOf(book.copy(sortCategory = null))
+                    else
+                        book.categories.asSequence().map { book.copy(sortCategory = it.category) }
+                }.flatten()
+            }) {
+            sequenceOf(categories.asSequence().map { it.category })
         },
-        ColumnValue(Column.TAGS, { if (tags.isNullOrEmpty()) null else tags[it.nextInt(tags.size)].name }) {
-            tags.asSequence().map { it.name }
+        ColumnValue(Column.TAGS, { if (tags.isNullOrEmpty()) null else tags[it.nextInt(tags.size)].name },
+            { (this.sortTag).compareWith(it.sortTag, true) },
+            {
+                map {book->
+                    if (book.tags.isEmpty())
+                        sequenceOf(book.copy(sortTag = null))
+                    else
+                        book.tags.asSequence().map { book.copy(sortTag = it.name) }
+                }.flatten()
+            }) {
+            sequenceOf(tags.asSequence().map { it.name })
         },
-        ColumnValue(Column.TITLE, { book.title }) {
-            sequenceOf(book.title)
+        ColumnValue(Column.TITLE, { book.title }, { book.title.compareWith(it.book.title) }) {
+            sequenceOf(sequenceOf(book.title))
         },
-        ColumnValue(Column.DATE_MODIFIED, { book.modified.dateValue(it) }) {
-            sequenceOf(book.modified)
+        ColumnValue(Column.DATE_MODIFIED, { book.modified.dateValue(it) }, { book.modified.compareTo(it.book.modified) }) {
+            sequenceOf(sequenceOf(book.modified))
         },
-        ColumnValue(Column.DATE_ADDED, { book.added.dateValue(it) }) {
-            sequenceOf(book.added)
+        ColumnValue(Column.DATE_ADDED, { book.added.dateValue(it) }, { book.added.compareTo(it.book.added) }) {
+            sequenceOf(sequenceOf(book.added))
         },
-        ColumnValue(Column.SUBTITLE, { book.subTitle }) {
-            sequenceOf(book.subTitle)
+        ColumnValue(Column.SUBTITLE, { book.subTitle }, { book.subTitle.compareWith(it.book.subTitle) }) {
+            sequenceOf(sequenceOf(book.subTitle))
         },
-        ColumnValue(Column.SOURCE_ID, { book.volumeId }) {
-            sequenceOf(book.volumeId)
+        ColumnValue(Column.SOURCE_ID, { book.volumeId }, { book.volumeId.compareWith(it.book.volumeId) }) {
+            sequenceOf(sequenceOf(book.volumeId))
         },
-        ColumnValue(Column.SOURCE, { book.sourceId }) {
-            sequenceOf(book.sourceId)
+        ColumnValue(Column.SOURCE, { book.sourceId }, { book.sourceId.compareWith(it.book.sourceId) }) {
+            sequenceOf(sequenceOf(book.sourceId))
         },
-        ColumnValue(Column.RATING, { book.rating.toString() }) {
-            sequenceOf(book.rating)
+        ColumnValue(Column.RATING, { book.rating.toString() }, { book.rating.compareTo(it.book.rating) }) {
+            sequenceOf(sequenceOf(book.rating))
         },
-        ColumnValue(Column.PAGE_COUNT, { book.pageCount.toString() }) {
-            sequenceOf(book.pageCount)
+        ColumnValue(Column.PAGE_COUNT, { book.pageCount.toString() }, { book.pageCount.compareTo(it.book.pageCount) }) {
+            sequenceOf(sequenceOf(book.pageCount))
         },
-        ColumnValue(Column.ISBN, { book.ISBN }) {
-            sequenceOf(book.ISBN)
+        ColumnValue(Column.ISBN, { book.ISBN }, { book.ISBN.compareWith(it.book.ISBN) }) {
+            sequenceOf(sequenceOf(book.ISBN))
         },
-        ColumnValue(Column.DESCRIPTION, { book.description }) {
-            sequenceOf(book.description)
+        ColumnValue(Column.DESCRIPTION, { book.description }, { book.description.compareWith(it.book.description) }) {
+            sequenceOf(sequenceOf(book.description))
         },
-        ColumnValue(Column.BOOK_COUNT, { book.bookCount.toString() }) {
-            sequenceOf(book.bookCount)
+        ColumnValue(Column.BOOK_COUNT, { book.bookCount.toString() }, { book.bookCount.compareTo(it.book.bookCount) }) {
+            sequenceOf(sequenceOf(book.bookCount))
         },
     ).also {array ->
         // Set the entry for the Column.Any column
-        array[0] = ColumnValue(Column.ANY, { anyValue(it) }) {
+        array[0] = ColumnValue(Column.ANY, { anyValue(it) }, { 0 }) {
             array.asSequence()
                 .filter { it.column != Column.ANY }  // Add more filters as needed
                 .map { it.allValues(this) }         // Convert array entries to value sequences
@@ -336,30 +466,40 @@ class BookFilterTest {
         {array -> this.convertAndTest(array, { cvtDate() }, { range -> range?.let { time >= it.first }?: false }) },
     )
 
-    private fun Sequence<Any?>.testValues(filterValues: Array<String>, predicate: PredicateTests): Boolean {
-        for (b in this) {
-            val test = when (b) {
-                is String -> { predicate.string.invoke(b, filterValues) }
-                is Int -> { predicate.int.invoke(b, filterValues) }
-                is Double -> { predicate.double.invoke(b, filterValues) }
-                is Date -> { predicate.date.invoke(b, filterValues) }
-                else -> { false }
+    private fun Sequence<Sequence<Any?>>.testValues(filterValues: Array<String>, predicate: PredicateTests): Boolean? {
+        var result: Boolean? = null
+        forEach {values ->
+            var count = 0
+            values.forEach {
+                ++count
+                if (it != null) {
+                    val test = when (it) {
+                        is String -> predicate.string.invoke(it, filterValues)
+                        is Int -> predicate.int.invoke(it, filterValues)
+                        is Double -> predicate.double.invoke(it, filterValues)
+                        is Date -> predicate.date.invoke(it, filterValues)
+                        else -> false
+                    }
+                    if (test)
+                        return true
+                    result = test
+                }
             }
-            if (test)
-                return true
+            if (count == 0)
+                result = false
         }
-        return false
+        return result
     }
 
     private val predicateValue = arrayOf(
-        PredicateValue(Predicate.ONE_OF, { this }) { testValues(it, oneOf) },
-        PredicateValue(Predicate.NOT_ONE_OF, { this }) { !testValues(it, oneOf) },
-        PredicateValue(Predicate.NOT_GLOB, { this?.randomSubstring(it) }) { !testValues(it, glob) },
-        PredicateValue(Predicate.GLOB, { this?.randomSubstring(it) }) { testValues(it, glob) },
-        PredicateValue(Predicate.LT, { this }) { testValues(it, lt) },
-        PredicateValue(Predicate.GT, { this }) { testValues(it, gt) },
-        PredicateValue(Predicate.LE, { this }) { testValues(it, le) },
-        PredicateValue(Predicate.GE, { this }) { testValues(it, ge) },
+        PredicateValue(Predicate.ONE_OF, { this }) { testValues(it, oneOf) == true },
+        PredicateValue(Predicate.NOT_ONE_OF, { this }) { testValues(it, oneOf) == false },
+        PredicateValue(Predicate.NOT_GLOB, { this?.randomSubstring(it) }) { testValues(it, glob) == false },
+        PredicateValue(Predicate.GLOB, { this?.randomSubstring(it) }) { testValues(it, glob) == true },
+        PredicateValue(Predicate.LT, { this }) { testValues(it, lt) == true },
+        PredicateValue(Predicate.GT, { this }) { testValues(it, gt) == true },
+        PredicateValue(Predicate.LE, { this }) { testValues(it, le) == true },
+        PredicateValue(Predicate.GE, { this }) { testValues(it, ge) == true },
     )
 
     private fun Sequence<BookAndAuthors>.filterSequence(c: ColumnValue, p: PredicateValue, values: Array<String>): Sequence<BookAndAuthors> {
